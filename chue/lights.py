01dd7ee723963a7e70384f860b10d8a10af3785a
Naren Mudivarthy <nmudivar@cisco.com>
2018-07-08 10:50:53 -0700

    integrated with python-fire

---
+++ chue/lights.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python3
+"""
+CLI module to control philips hue lights.
+"""
+import phue
+import fire
+
+
+BRIDGE_IP = "10.0.1.47"
+# BRIDGE_IP = "10.0.1.6"
+
+class Light(object):
+
+    def __init__(self):
+        self.bridge = phue.Bridge(BRIDGE_IP)
+        self.bridge.connect()
+        phue.logger.propagate = False
+        phue.logger.disabled = False
+
+    def print(self, *args, **kwargs):
+        print(*args, **kwargs)
+
+    def action_on_light_by_id(self, light_id, action):
+        """
+        Action on one light by light_id.
+        """
+        if action == 'on': self.bridge.set_light(light_id, 'on', True)
+        else:  self.bridge.set_light(light_id, 'on', False)
+
+        return
+
+    def action_on_group_of_lights(self, lights, action):
+        """
+        Action on one light by light_id.
+        """
+        for l in lights:
+            if action == 'on':
+               self.bridge.set_light(l.light_id, 'on', True)
+            else : self.bridge.set_light(l.light_id, 'on', False)
+
+        return
+
+    def on(self, light_id=None):
+        if light_id:
+            l = int(light_id )
+            result = self.bridge.set_light(l, 'on', True)
+            self.print('Turning on light %s: Status %s' % (l, result ))
+        else:
+            self.print('Turning on all lights')
+            for l in self.bridge.lights:
+                self.action_on_light_by_id(l.light_id, 'on' )
+
+    def off(self, light_id = None):
+        if light_id:
+            l = int(light_id)
+            self.print('Turning off light %s' % l)
+            self.bridge.set_light(l, 'off', True)
+        else:
+            self.print('Turning off all lights')
+            for l in self.bridge.lights:
+                self.action_on_light_by_id(l.light_id, 'off' )
+
+
+if __name__ == '__main__':
+    fire.Fire(Light)
